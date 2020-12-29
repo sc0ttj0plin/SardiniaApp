eb95a113617f96dd8abff51c73c4c031dbc3288f
Samuel Aldo Iacolina <samuel@crs4.it>
2020-12-29 11:48:42 +0100

    iPad optimization

--- components/EntityRelatedList.js
+++ components/EntityRelatedList.js
@@ -124,7 +124,7 @@ export default class EntityRelatedList extends PureComponent {
                         style={[styles.fill, this.props.style]}
                         contentContainerStyle={contentContainerStyle}
                         showsHorizontalScrollIndicator={showsHorizontalScrollIndicator || true}
-                        initialNumToRender={4} // Reduce initial render amount
+                        initialNumToRender={6} // Reduce initial render amount
                         updateCellsBatchingPeriod={400} // Increase time between renders
                         windowSize={10} // Reduce the window size
                         ItemSeparatorComponent={() => <View style={{height: 10}}></View>}
